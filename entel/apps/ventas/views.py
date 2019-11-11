92a4cc3aa2825a377386703e2bb5c50c0c5e50dd
Christian <therishling@gmail.com>
2019-11-11 01:59:31 -0300

    Cambios diseño

--- entel/apps/ventas/views.py
+++ entel/apps/ventas/views.py
@@ -1,21 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from django.urls import reverse,reverse_lazy
 from apps.ventas import models as modelos
 from apps.ventas import forms as formularios
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
 def index(request):
-    user = request.user
-    if user.admin:
-        return render(request, 'dashboard/index.html')
     return render(request, 'dashboard/index.html')
 
-class CrearUsuario(SuccessMessageMixin, CreateView):
+
+
+class CrearUsuario(UserPassesTestMixin, SuccessMessageMixin, CreateView):
+
     model = modelos.Vendedor
     form_class = formularios.CrearUsuario
     template_name = 'dashboard/admin/crearvendedor.html'
     success_message = 'Usuario registrado correctamente'
     success_url = reverse_lazy('crear usuario')
+    
+    def test_func(self, redirect_field_name='dashboard'):
+        user = self.request.user
+        return user.admin
+    
+    def handle_no_permission(self):
+        return redirect('dashboard')
+        
+
+    
+        
--- entel/entel/urls.py
+++ entel/entel/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.ventas.urls'), name='ventas'),
-    path('', auth_views.LoginView.as_view(template_name='login/index.html', redirect_authenticated_user= True), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login/index.html', redirect_authenticated_user = True), name='login'),
     path('logout/', auth_views.logout_then_login , name='logout')
 ]
